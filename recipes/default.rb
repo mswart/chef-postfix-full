8c03e3231f7cfa93da9f7aba9f92148cda62e44a
Xabier de Zuazo <xabier@onddo.com>
2013-09-09 17:43:19 +0200

    chroot etc directory creation fixed to take into account the queue_directory attribute

--- recipes/default.rb
+++ recipes/default.rb
@@ -70,17 +70,20 @@ used_table_types.uniq.each do |table_type|
   package pkg
 end
 
-directory '/var/spool/postfix/etc' do
-  owner 'root'
-  group 'root'
-  mode '0755'
-end
+postfix_chroot = node['postfix']['main']['queue_directory'] || '/var/spool/postfix'
 
 # copy required files to the chroot dir
-postfix_chroot = node['postfix']['main']['queue_directory'] || '/var/spool/postfix'
 node['postfix']['chroot_files'].each do |path, action|
   case action
   when 'cp'
+    directory_path = ::File.dirname(path)
+    directory "#{postfix_chroot}/#{directory_path}" do
+      owner 'root'
+      group 'root'
+      mode '0755'
+      recursive true
+      only_if { directory_path.length > 0 and not ::File.exists?("#{postfix_chroot}/#{directory_path}") }
+    end
     file_exists = ::File.exists?("/#{path}")
     file_content = file_exists ? IO.read("/#{path}") : nil # avoid ENOENT error
     file "#{postfix_chroot}/#{path}" do
